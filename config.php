9c39ea5f2a88d5a3e04967dbd7de142304386a8f
Ashikur Rahman <32567010+ashik3g@users.noreply.github.com>
2022-06-20 15:08:00 +0600

    Admin Login Done

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php 
+$host = "localhost";
+$db_name = "psms";
+$user = "root";
+$password = "";
+
+date_default_timezone_set("Asia/Dhaka");
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+}
+catch(PDOException $m){
+    echo "Connection failed: " . $m->getMessage();
+}
+
+
+
+// Count any Column Value from Students Table
+// function stRowCount($col,$val){
+//     global $pdo;
+//     $stm=$pdo->prepare("SELECT $col FROM students WHERE $col=?");
+//     $stm->execute(array($val));
+//     $count = $stm->rowCount();
+//     return $count;
+// }
+
+//  GET Student Data
+// function Student($col,$id){
+//     global $pdo;
+//     $stm=$pdo->prepare("SELECT $col FROM students WHERE id=?");
+//     $stm->execute(array($id));
+//     $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+//     return $result[0][$col];
+// }
+
